e1c468413efa2e3c9f68aad15668074ecce8bb8d
arandomuser0 <90565424+arandomuser0@users.noreply.github.com>
2023-09-26 16:09:00 -0300

    Update 77_0NOYKVH.lua

--- 77_0NOYKVH.lua
+++ 77_0NOYKVH.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/arandomuser0/scriptroblox/main/77_XNJCHFEU.lua))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/arandomuser0/scriptroblox/main/77_XNJCHFEU.lua"))()
